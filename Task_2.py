fd700430db01ab1ba5b14090aec3b16c1db285a1
Angelina <chulakovaav@yandex.ru>
2022-11-30 14:59:19 +0300

    corrected task_2

--- Task_2.py
+++ Task_2.py
@@ -6,7 +6,7 @@
 
 def list_completion(my_list: list, length: int):
     for i in range(0, length):
-        my_list.append(random.randint(1, 10))
+        my_list.append(random.randint(1, 6))
     # print(my_list)
     return my_list
 
@@ -16,15 +16,15 @@ def search_sum(my_list: list):
     index_pos = 0
     index_neg = -1
     while index_pos < len(my_list) / 2:
-        sum_number = 0
-        sum_number = my_list[index_pos] + my_list[index_neg]
-        new_list.append(sum_number)
+        mult_number = my_list[index_pos] * my_list[index_neg]
+        new_list.append(mult_number)
         index_pos += 1
         index_neg -= 1
     return new_list
 
 
 first_list = []
-list_length = random.randint(4, 6)
+list_length = random.randint(4, 8)
 x = list_completion(first_list, list_length)
 print(f"{x} => {search_sum(x)}")
+
